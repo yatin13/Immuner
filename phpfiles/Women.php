a777f25b19c281fcbc639e467b9e495ed0c9fefb
yatin13 <78989274+yatin13@users.noreply.github.com>
2021-06-10 00:33:39 +0530

    Add files via upload

--- phpfiles/Women.php
+++ phpfiles/Women.php
@@ -53,69 +53,35 @@
         <img class="w3-image" src="../Media/womenback10.jpeg">
       </div>
     </div>
-    <div class="row s">
-  <div class="col-sm-4">
-  <div class="card border-info" style="width: 18rem;">
-  <img src="../Media/womenprod6.webp" id="img1" class="card-img-top" alt="...">
-  <div class="card-body text-primary">
-    <h5 class="card-title" id="pd1">LEAN WOMEN WHEY</h5>
-    <p class="starrating card-text"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i></p>
-    <h4 class="text-danger" id="rup1"><s>MRP: ₹1,699</s> 
-    <br>Rs.1500 | 11% off</h4>
-    <button class="btn btn-outline-primary btn3" id="btn1" data="1" value="../Media/womenprod6.webp ,Rs1500, LEAN WOMEN WHEY">Add to Cart</button>
+  <?php 
+   function renderProducts(){
+    $con=mysqli_connect("localhost","root","","imagestable");
+    $output='';
+    $sql=mysqli_query($con,'SELECT * from tableforwomen');
+    while($row=mysqli_fetch_array($sql))
+    {
+     $output.='<div class="col-sm-4">
+               <div class="card border-info" style="width: 18rem;">
+               <img src="data:image;base64,'.base64_encode($row[3]).'" width="420" height="300" class="card-img-top" alt="...">
+               <div class="card-body text-primary">
+               <h5 class="card-title" id="pd1" >'.$row[1].'</h5>
+               <p class="card-text">'.$row[4].'</p>
+               <button class="btn btn-primary" id="btn1" data='.$row[0].'value="data:image;base64,'.base64_encode($row[3]). '",Rs.1500, IMMUNERPROTINO">Add to Cart</button>
+               <h4 class="text-danger" id="rup1">'.$row[2].'</h4>
+               </div>
+               </div>
+               </div>';
+    }
+    return $output;
+    }
+    ?>
+  <div class="row s allproduct" style='display:grid;grid-template-columns:auto auto auto;grid-gap:1rem;'>
+  <?php 
+  echo renderProducts();
+  ?>
   </div>
-</div>
-</div>
- <div class="col-sm-4">
-<div class="card border-info" style="width: 18rem;">
-  <img src="../Media/womenprod5.webp" id="img2" style="height:18rem" class="card-img-top" alt="...">
-  <div class="card-body text-primary">
-    <h5 class="card-title" id="pd2">IMMUNER DELIGHT</h5>
-    <p class="starrating  card-text"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i></p>
-    <h4 class="text-danger" id="rup2"><s>MRP: ₹1,349</s> 
-    <br>Rs.1111 | 17% off</h4></h4>
-    <button class="btn btn-outline-primary btn3" data="2" value="../Media/womenprod5.webp ,Rs1111 , IMMUNER DELIGHT" id="btn2">Add to Cart</button>
-    </div>
-</div>
-</div>
-<div class="col-sm-4">
-  <div class="card border-info" style="width: 18rem;">
-  <img src="../Media/womenprod2.webp" id="img3" style="height:18rem" class="card-img-top" alt="...">
-  <div class="card-body text-primary">
-    <h5 class="card-title" id="pd3">IMMUNER PROTINO SUPER</h5>
-    <p class="starrating card-text"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i></p>
-   <h4 class="text-danger" id="rup3"><s>MRP: ₹699</s>Rs.664 | 5% off</h4>
-    <button class="btn btn-outline-primary btn3" id="btn3" data="3" value="../Media/womenprod2.webp ,Rs664 , IMMUNER PROTINO SUPER ">Add to Cart</button>
-  </div>
-</div>
-</div>
-</div>
-<div class="row s">
-  <div class="col-sm-4 s1">
-  <div class="card border-info" style="width: 18rem;">
-  <img src="../Media/womenprod3.webp" class="img-prod" style="height:18rem" class="card-img-top" alt="...">
-  <div class="card-body text-primary">
-    <h5 class="card-title" id="pd4">IMMUNER SLIMMING</h5>
-    <p class="starrating card-text"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i></p>
-     <h4 class="text-danger" id="rup4" data="Rs 1500"><s>MRP: ₹1299</s>| Rs.1240 5.6% Off</h4>
-    <button class="btn btn-outline-primary btn3" id="btn4" data="4" value="../Media/womenprod3.webp ,Rs1240, IMMUNER SLIMMING">Add to Cart</button>
-    </div>
-</div>
-</div>
- <div class="col-sm-4 s2">
-  <div class="card border-info" style="width: 18rem;">
-  <img src="../Media/womenprod4.webp" id="img4" style="height:18rem" class="card-img-top" alt="...">
-  <div class="card-body text-primary">
-    <h5 class="card-title" id="pd4">IMMUNER NOURIZA</h5>
-    <p class="starrating card-text"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i></p>
-     <h4 class="text-danger" id="rup5" data="Rs 1500"><s>MRP: ₹1499</s>Rs.1375 | 8% off</h4>
-    <button class="btn btn-outline-primary btn3" id="btn4" data="4" value="../Media/womenprod4.webp ,Rs1375, IMMUNER NOURIZA">Add to Cart</button>
-    </div>
-</div>
-</div>
-</div>
 <div class="gif">
-<h4 style="color:#FF1493;margin-left:12rem;">Right section for fitness ladies<img src="muscles.png" width="100" height="100"/></h4>
+<h4 style="color:#FF1493;margin-left:12rem;">Right section for fitness ladies<img src="../Media/muscles.png" width="100" height="100"/></h4>
 <iframe src="https://giphy.com/embed/0MfcxV0IymysyAKrdE" style="margin-left:12rem;" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>
 <img width="400" height="400" style="margin-top:-25rem ;" src="../Media/dietwomen.jpeg"> 
 </div>
